9d03c3fc0dbe7cf95b43f23479aee95befe1927b
jmorton138 <jmorton138@gmail.com>
2021-06-08 18:45:55 +0100

    Added multiple rounds score ttracker

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let userStatus = "";
+
 //computer's choice, random out of 3 options
 function computerPlay() {
     const choice = Math.floor(Math.random()*3);
@@ -35,40 +37,67 @@ function playRound(userChoice, compsChoice) {
     console.log("Computer chose: " + compsChoice);
     if (userChoice == compsChoice) {
         alert("draw");
+        return userStatus ="draw";
     } else if (userChoice == "rock") {
         if (compsChoice == "scissors") {
             console.log("You win! Rock beats scissors.");
-            return;
+            return userStatus = "You win!";
         } else if (compsChoice == "paper") {
             console.log("You lose! Paper beats rock.");
-            return;
+            return userStatus = "You lose!";
         }
     } else if (userChoice == "paper") {
         if (compsChoice == "rock") {
             console.log("You win! Paper beats rock.");
-            return;
+            return userStatus ="You win!";
         } else if (compsChoice == "scissors") {
             console.log("You lose! Scissors beats paper.");
-            return;
+            return userStatus ="You lose!";
         }
     } else if (userChoice == "scissors") {
         if (compsChoice == "paper") {
             console.log("You win! Scissors beats paper.");
-            return;
+            return userStatus ="You win!";
         } else if (compsChoice == "rock") {
             console.log("You lose! Rock beats scissors.");
-            return;
+            return userStatus ="You lose!";
         }
     }
 
 }
 
 function game() {
-    
+    //set score counters
+    let userScore = 0;
+    let compScore = 0;
+    //play 5 rounds
+    for (i = 0; i <= 4; i++){
+        playerSelection();
+        
+   
+        //keep track of each player's score by updating score each round
+        if (userStatus == "draw") {
+            i--;
+        } else if (userStatus == "You win!") {
+            userScore++;
+            console.log("you: " + userScore);
+        } else if (userStatus == "You lose!") {
+            compScore++;
+            console.log("comp: " + compScore);
+        }
+
+    }
+    //decide winner after 5 rounds
+    if (userScore > compScore) {
+        console.log("You won everything!");  
+    } else {
+        console.log("You lost everything");
+    }
+    console.log("Final score: " + "You: " + userScore + "comp: " + compScore);
 }
 
 
-playerSelection();
+game();
 
 
 
